4f03a2ca46a2cb4d03f6a6f454c40150d9be671c
arkarlin2003 <arkarlin486@gmail.com>
2025-03-10 11:47:28 +0630

    feat(app): container-component

---
+++ container-components/src/layouts/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import SplitScreen from "./components/split-screen";
+import Modal from "./components/modal";
+
+const LeftSide = ({ title }: { title: string }) => {
+  const [active, setActive] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setActive(true)}>Open Modal</button>
+      <h1 style={{ backgroundColor: "red" }}>I am {title}!</h1>
+      <Modal opened={active} close={() => setActive(false)}>
+        <div >
+          <h1>Hello Modal</h1>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+const RightSide = ({ title }: { title: string }) => {
+  return <h1 style={{ backgroundColor: "blue" }}>I am {title}!</h1>;
+};
+
+const App = () => {
+  return (
+    <SplitScreen leftWidth={1} rightWidth={1}>
+      <LeftSide title="Left" />
+      <RightSide title="Right" />
+    </SplitScreen>
+  );
+};
+
+export default App;
