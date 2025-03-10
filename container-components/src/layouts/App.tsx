cafe283ec3785067453b3ce472625f621ff5b7e8
arkarlin2003 <arkarlin486@gmail.com>
2025-03-10 13:12:31 +0630

    feat(app): user loader with user info

--- container-components/src/layouts/App.tsx
+++ container-components/src/layouts/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import SplitScreen from "./components/split-screen";
 import Modal from "./components/modal";
+import UserLoader from "./components/container-components/user-loader";
+import UserInfo from "./components/container-components/user-info";
 
 const LeftSide = ({ title }: { title: string }) => {
   const [active, setActive] = useState(false);
@@ -9,7 +11,7 @@ const LeftSide = ({ title }: { title: string }) => {
       <button onClick={() => setActive(true)}>Open Modal</button>
       <h1 style={{ backgroundColor: "red" }}>I am {title}!</h1>
       <Modal opened={active} close={() => setActive(false)}>
-        <div >
+        <div>
           <h1>Hello Modal</h1>
         </div>
       </Modal>
@@ -17,15 +19,30 @@ const LeftSide = ({ title }: { title: string }) => {
   );
 };
 
-const RightSide = ({ title }: { title: string }) => {
-  return <h1 style={{ backgroundColor: "blue" }}>I am {title}!</h1>;
+const RightSide = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => {
+  return (
+    <>
+      <h1 style={{ backgroundColor: "blue" }}>I am {title}!</h1>
+      {children}
+    </>
+  );
 };
 
 const App = () => {
   return (
     <SplitScreen leftWidth={1} rightWidth={1}>
       <LeftSide title="Left" />
-      <RightSide title="Right" />
+      <RightSide title="Right">
+        <UserLoader resourceUrl="https://fakestoreapi.com/users/4">
+          <UserInfo />
+        </UserLoader>
+      </RightSide>
     </SplitScreen>
   );
 };
