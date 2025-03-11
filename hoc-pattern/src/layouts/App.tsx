f2a18417aade4bc685a496603674c676cb877ddd
arkarlin2003 <arkarlin486@gmail.com>
2025-03-11 15:42:12 +0630

    feat(app): hoc pattern

---
+++ hoc-pattern/src/layouts/App.tsx
@@ -0,0 +1,119 @@
+import { ReactNode, useState } from "react";
+import SplitScreen from "./components/split-screen";
+import Modal from "./components/modal";
+import UserLoader from "./components/container-components/user-loader";
+import UserInfo from "./components/container-components/user-info";
+import UncontrolledFlow from "./components/uncontrolled-controlled-components/uncontrolled-flow";
+import ControlledFlow from "./components/uncontrolled-controlled-components/controlled-flow";
+
+const LeftSide = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => {
+  const [active, setActive] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setActive(true)}>Open Modal</button>
+      <h1 style={{ backgroundColor: "red" }}>I am {title}!</h1>
+      <Modal opened={active} close={() => setActive(false)}>
+        {children}
+      </Modal>
+    </div>
+  );
+};
+
+const RightSide = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => {
+  return (
+    <>
+      <h1 style={{ backgroundColor: "blue" }}>I am {title}!</h1>
+      {children}
+    </>
+  );
+};
+
+const StepOne = ({ onNext }: { onNext?: (e: { name: string }) => void }) => {
+  return (
+    <div>
+      <h1>Step One</h1>
+      <button onClick={() => onNext({ name: "one" })}>Next</button>
+    </div>
+  );
+};
+
+const StepTwo = ({ onNext }: { onNext?: (e: { name: string }) => void }) => {
+  return (
+    <div>
+      <h1>Step Two</h1>
+      <button onClick={() => onNext({ name: "two" })}>Next</button>
+    </div>
+  );
+};
+
+const StepThree = ({ onNext }: { onNext?: (e: { name: "three" }) => void }) => {
+  return (
+    <div>
+      <h1>Step Three</h1>
+      <button onClick={() => onNext({ name: "three" })}>Next</button>
+    </div>
+  );
+};
+
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState<{ name: string }[]>([]);
+  return (
+    <>
+      <SplitScreen leftWidth={1} rightWidth={1}>
+        <LeftSide title="Left">
+          <UserLoader resourceUrl="https://fakestoreapi.com/users/2">
+            <UserInfo />
+          </UserLoader>
+        </LeftSide>
+        <RightSide title="Right">
+          <UserLoader resourceUrl="https://fakestoreapi.com/users/4">
+            <UserInfo />
+          </UserLoader>
+        </RightSide>
+      </SplitScreen>
+
+      <UncontrolledFlow
+        onDone={(data) => {
+          console.log(data);
+        }}
+      >
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledFlow>
+
+      <ControlledFlow
+        currentIndex={currentIndex}
+        nextStep={setCurrentIndex}
+        onDone={(d) => {
+          setData(d);
+        }}
+      >
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </ControlledFlow>
+
+      <ul>
+        {data.map((d) => (
+          <li key={d.name}>{d.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
